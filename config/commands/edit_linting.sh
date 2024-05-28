f7487a0f9e0e12e719f8e79ad2d8551a0f89ec58
Mayank Dave <mayank.dave@codenation.co.in>
2024-05-29 00:08:42 +0530

    add linter for xml

--- config/commands/edit_linting.sh
+++ config/commands/edit_linting.sh
@@ -70,6 +70,8 @@ edit() {
     # Run linter
     if [[ $CURRENT_FILE == *.py ]]; then
         lint_output=$(flake8 --isolated --select=F821,F822,F831,E111,E112,E113,E999,E902 "$CURRENT_FILE" 2>&1)
+    elif [[ $CURRENT_FILE == *.xml ]]; then
+        lint_output=$(xmllint --noout --nowarning "$CURRENT_FILE" 2>&1)
     else
         # do nothing
         lint_output=""
